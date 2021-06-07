6220d8185c624e6349b4a28edbbb5fdc86d11328
sharunas11 <85257835+sharunas11@users.noreply.github.com>
2021-06-07 19:05:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function submit_form() {
+    document.formu1.submit();
+    document.formu1.reset(); 
+    }
+
+
+document.getElementById("demo").innerHTML = "Hello World!";
+document.getElementById("demo").innerHTML = Date();
